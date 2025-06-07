6beafa52f8a4d5203c534fb3aa2c60069944d185
Droid-MAX <Droid-MAX@users.noreply.github.com>
2025-06-07 20:42:46 +0800

    update forward.sh

--- forward.sh
+++ forward.sh
@@ -8,7 +8,14 @@ fi
 
 inbond_iface="usb0"
 outbond_iface="eth0"
-inbond_cidr_addr=`ip -4 -br addr show $inbond_iface | tr -s ' ' | cut -d' ' -f3`
+
+check_inbond_addr(){
+	inbond_cidr_addr=$(ip -4 -br addr show $inbond_iface | tr -s ' ' | cut -d' ' -f3)
+	if [ -z "$inbond_cidr_addr" ]; then
+		echo "[!] could not get ip address for interface $inbond_iface"
+		exit 1
+	fi
+}
 
 check_ipv4_forward(){
 	if [ "$(cat /proc/sys/net/ipv4/ip_forward)" -eq "1" ]; then
@@ -27,17 +34,19 @@ enable_ipv4_forward(){
 }
 
 check_iptables_rule_exists(){
-    local forward_rule_1=$(iptables -C FORWARD -i $inbond_iface -o $outbond_iface -j ACCEPT 2>/dev/null; echo $?)
-    local forward_rule_2=$(iptables -C FORWARD -i $outbond_iface -o $inbond_iface -m state --state RELATED,ESTABLISHED -j ACCEPT 2>/dev/null; echo $?)
-    local nat_rule=$(iptables -t nat -C POSTROUTING -s $inbond_cidr_addr -o $outbond_iface -j MASQUERADE 2>/dev/null; echo $?)
-    if [ "$forward_rule_1" -eq 0 ] && [ "$forward_rule_2" -eq 0 ] && [ "$nat_rule" -eq 0 ]; then
-        echo 0
-    else
-        echo 1
-    fi
+	check_inbond_addr
+	local forward_rule_1=$(iptables -C FORWARD -i $inbond_iface -o $outbond_iface -j ACCEPT 2>/dev/null; echo $?)
+	local forward_rule_2=$(iptables -C FORWARD -i $outbond_iface -o $inbond_iface -m state --state RELATED,ESTABLISHED -j ACCEPT 2>/dev/null; echo $?)
+	local nat_rule=$(iptables -t nat -C POSTROUTING -s $inbond_cidr_addr -o $outbond_iface -j MASQUERADE 2>/dev/null; echo $?)
+	if [ "$forward_rule_1" -eq 0 ] && [ "$forward_rule_2" -eq 0 ] && [ "$nat_rule" -eq 0 ]; then
+		echo 0
+	else
+		echo 1
+	fi
 }
 
 add_iptables_rules(){
+	check_inbond_addr
 	pre_existing_rule=$(check_iptables_rule_exists)
 	if [ "$pre_existing_rule" -eq 1 ]; then
 		iptables -A FORWARD -i $inbond_iface -o $outbond_iface -j ACCEPT
@@ -50,6 +59,7 @@ add_iptables_rules(){
 }
 
 remove_iptables_rules(){
+	check_inbond_addr
 	pre_existing_rule=$(check_iptables_rule_exists)
 	if [ "$pre_existing_rule" -eq 0 ]; then
 		iptables -D FORWARD -i $inbond_iface -o $outbond_iface -j ACCEPT
@@ -79,11 +89,12 @@ do_disable(){
 }
 
 usage(){
-    echo "Usage: $0 {enable|disable} [--inbond <interface>] [--outbond <interface>]"
+    echo "Usage: $0 {enable|disable} [-i/--inbond <interface>] [-o/--outbond <interface>]"
     echo "Options:"
-    echo "  enable/disable      Enable or disable forwarding (required)"
-    echo "  --inbond <iface>    Specify inbound interface (default: usb0)"
-    echo "  --outbond <iface>   Specify outbound interface (default: eth0)"
+    echo "  enable/disable	Enable or disable forwarding (required)"
+    echo "  -i, --inbond <iface>	Specify inbound interface (default: usb0)"
+    echo "  -o, --outbond <iface>	Specify outbound interface (default: eth0)"
+    echo "  -h, --help		Get help for commands"
     exit 1
 }
 
@@ -93,11 +104,11 @@ while [[ $# -gt 0 ]]; do
             action=$1
             shift
             ;;
-        --inbond)
+        -i|--inbond)
             inbond_iface=$2
             shift 2
             ;;
-        --outbond)
+        -o|--outbond)
             outbond_iface=$2
             shift 2
             ;;
